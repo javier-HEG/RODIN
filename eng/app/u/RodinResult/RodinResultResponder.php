b1eed0d39184ff61ac0f101d9cb80667b847d87b
Javier Belmonte <javier.belmonte@hesge.ch>
2013-01-14 12:05:25 +0100

    Repair the result aggregation - Display the correct aggregation toggle status when changing tabs - If Solr is not used then don't test it at system start

--- eng/app/u/RodinResult/RodinResultResponder.php
+++ eng/app/u/RodinResult/RodinResultResponder.php
@@ -7,9 +7,7 @@ $sid = $_POST['sid'];
 // The suffix is used to distinguish the header and
 // content divs from results in different tabs
 $suffix = $_POST['suffix'];
-
 $fromResult = isset($_POST['from']) ? $_POST['from'] : 0;
-
 $jsonAllResults = array();
 
 //$allResults = RodinResultManager::getRodinResultsFromSOLRForASearch($sid);
@@ -54,6 +52,7 @@ while ($i < $uptoResult) {
 		break;
 	
 	$jsonAllResults[] = $jsonSingleResult;
+	$i++;
 }
 
 header('Content-type: application/json; charset=utf-8');
